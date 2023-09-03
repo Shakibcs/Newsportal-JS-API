ba0d83b54ca772644bddfbb2ecde534ccfb25d0e
Shakibul Alam <droid@Shakibuls-Air.station>
2023-09-03 19:22:37 +0100

    news category fetch & show data task done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 const handleCategory = async() => {
     const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
     const data = await response.json();
-    console.log(data.data.news_category);
+
+    const tabContainer = document.getElementById('tab-container');//get html data
+
+    data.data.news_category.slice(0,3).forEach((category)=>{ //get data with foreach loop
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <a class="tab">${category.category_name}</a>       
+        `;
+        tabContainer.appendChild(div);                    //append child to container
+    });
+
+    
+    //console.log(data.data.news_category);
 }
 
 handleCategory();
