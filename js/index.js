4287977c43a6bd42239da380baff142584c4aa56
Shakibul Alam <droid@Shakibuls-Air.station>
2023-09-03 18:55:22 +0100

    JS functionlity added to fetch and convert api to json

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
+const handleCategory = async() => {
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json();
+    console.log(data);
+}
 
+handleCategory();
 
 
     
